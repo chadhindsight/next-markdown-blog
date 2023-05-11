45e34a4f2d28f0fbc57898baacd0a0994e7339ce
chad hinds <chadrickj8@gmail.com>
2023-05-11 12:52:30 -0400

    Setup of getStaticPaths

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -8,7 +8,20 @@ export default function PostPage() {
 }
 
 export async function getStaticPaths() {
+    // Get posts 
+    const files = fs.readdirSync(path.join('posts'))
 
+    const paths = files.map((filename) => ({
+        params: {
+            slug: filename.replace('.md', ''),
+        },
+    }))
+
+    return {
+        // array of accessible paths
+        paths,
+        fallback: false,
+    }
 
 }
 // Next.js will pre-render a page at build time using these props
