c3327d20d9846a632bfc6dbb9780dff6c3884d6f
chad hinds <chadrickj8@gmail.com>
2023-05-18 13:17:06 -0400

    Update slug

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,45 +1,29 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
 import matter from 'gray-matter'
 import marked from 'marked'
 import Link from 'next/link'
 
-export default function PostPage() {
+export default function PostPage({
+    frontmatter: { title, date, cover_image },
+    slug,
+    content,
+}) {
     return (
         <>
-            <Link href="/">
+            <Link href='/'>
                 <a className='btn btn-back'>Go Back</a>
             </Link>
             <div className='card card-page'>
                 <h1 className='post-title'>{title}</h1>
-                <div className='post-date'>Posted on {date}
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img src={cover_image} alt='Related visual aid for the article' />
-                    <div className='post-body'>
-                        <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
-                    </div>
+                <div className='post-date'>Posted on {date}</div>
+                <img src={cover_image} alt='' />
+                <div className='post-body'>
+                    <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
                 </div>
             </div>
         </>
     )
-}
-
-export async function getStaticProps({ params: { slug } }) {
-    const markdownWithMeta = fs.readFileSync(
-        path.join('posts', slug + '.md'),
-        'utf-8'
-    )
-
-    const { data: frontmatter, content } = matter(markdownWithMeta)
-
-    return {
-        // Next.js pre-renders a page at build time using these props
-        props: {
-            frontmatter,
-            slug,
-            content,
-        },
-    }
 }
 
 export async function getStaticPaths() {
@@ -54,5 +38,22 @@ export async function getStaticPaths() {
     return {
         paths,
         fallback: false,
+    }
+}
+
+export async function getStaticProps({ params: { slug } }) {
+    const markdownWithMeta = fs.readFileSync(
+        path.join('posts', slug + '.md'),
+        'utf-8'
+    )
+
+    const { data: frontmatter, content } = matter(markdownWithMeta)
+
+    return {
+        props: {
+            frontmatter,
+            slug,
+            content,
+        },
     }
 }
